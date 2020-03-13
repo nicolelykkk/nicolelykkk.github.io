147c15937af887b7642da444ad04fa3e599f83d8
YUKE LI <yukeli@YUKEdeMacBook-Pro.local>
2020-03-12 21:34:37 -0400

    classssss

---
+++ homework/cats/chart1.js
@@ -0,0 +1,19 @@
+d3.select("#barchart")
+    .on("mousemove", function() {
+
+        console.log(d3.event);
+
+});
+
+d3.select("#barchart")
+    .on("mousemove", function() {
+        d3.select("#tooltip")
+        .style("display", "block")
+        .style("top", d3.event.pageY + 20 + "px")
+        .style("left", d3.event.pageX + 20 + "px")
+        .html("Welcome to the first chart!");
+    })
+    .on("mouseout", function() {
+        d3.select("#tooltip")
+            .style("display", "none");
+});
