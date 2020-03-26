a0405ff9925eb68a9b82506b16c53e85d7bc245d
YUKE LI <yukeli@YUKEdeMacBook-Pro.local>
2020-03-26 15:20:53 -0400

    layout

--- homework/Final/script.js
+++ homework/Final/script.js
@@ -1,4 +1,4 @@
-URL = "https://coronavirus-19-api.herokuapp.com/countries";
+URL = "https://api.covid19api.com/summary";
 
 function fetchData() {
 
@@ -14,26 +14,67 @@ function fetchData() {
 
 fetchData(); 
 
-// d3.csv("./data/covid_19.csv").then(function(data) {
 
-//     var width = document.querySelector("#chart").clientWidth;
-//     var height = document.querySelector("#chart").clientHeight;
-//     var margin = {top: 50, left: 150, right: 50, bottom: 150};
+var svg = d3.select("#viz")
+        .attr("width", width)
+        .attr("height", height);
 
-//     var filtered_data = data.filter(function(d) {
-//         return d.Country === "Japan";
-//     })
-//     console.log(filtered_data);
+    svg.select("#ocean")
+    .attr("width", width)
+    .attr("height", height);
 
-//     var svg = d3.select("#chart")
-//         .append("svg")
-//         .attr("width", width)
-//         .attr("height", height);
+    var map = svg.select("#map");
 
-//     var Amount = {
-//         min: d3.min(filtered_data, function(d) { return +d.lifeExp; }),
-//         max: d3.max(filtered_data, function(d) { return +d.lifeExp; })
-//     };
+    var zoom = d3.zoom()
+    .translateExtent([
+        [0, 0],
+        [width, height]
+    ])
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
 
+    function zoomed() {
+    map.attr("transform", d3.event.transform);
+    }
 
-// });
+    svg.call(zoom)
+    .on("dblclick.zoom", null);
+
+    d3.json("../topojson/world-alpha3.json", function(error, world) {
+
+    var geoJSON = topojson.feature(world, world.objects.countries);
+
+    geoJSON.features = geoJSON.features.filter(function(d) {
+        return d.id !== "ATA";
+    });
+
+    var projection = d3.geoMercator()
+        .fitSize([width, height], geoJSON);
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+    var countries = map.selectAll("path")
+        .data(geoJSON.features);
+
+    countries.enter().append("path")
+        .attr("d", path)
+        .attr("fill", "#004400")
+        .attr("stroke", "#008800");
+
+    var points = [
+        {"name": "Boston", "coords": [-71.0589, 42.3601]},
+        {"name": "London", "coords": [-0.1278, 51.5074]}
+    ];
+
+    var circles = map.selectAll("circle")
+        .data(points);
+
+    circles.enter().append("circle")
+        .attr("transform", function(d) {
+        return "translate(" + projection(d.coords) + ")";
+        })
+        .attr("r", 10)
+        .attr("fill", "#cc0000");
+
+    });
